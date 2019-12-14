904036761eeee16e6aaee304f93fb50cccd7b50a
Wikipedia Brown <wikipediabrown@iamgoodbad.com>
2019-12-14 10:37:19 -0800

    added new junkm

--- JekyllScript.sh
+++ JekyllScript.sh
@@ -24,11 +24,11 @@ ABOUT_MD="---\r\ntitle: About\r\n---\r\n# About page\r\n\r\nThis page tells you 
 add_file . about.md "$ABOUT_MD" 'About'
 
 # Adding Initial Blog File
-BLOG_HTML="---\r\ntitle: Blog\r\n---\r\n<h1>Latest Posts</h1>\r\n\r\n<ul>\r\n  {%% for post in site.posts %%}\r\n    <li>\r\n      <h2><a href=\"{{ post.url }}\">{{ post.title }}</a></h2>\r\n      <p>{{ post.excerpt }}</p>\r\n    </li>\r\n  {%% endfor %%}\r\n</ul>"
+BLOG_HTML="---\r\ntitle: Blog\r\n---\r\n<h1>Latest Posts</h1>\r\n\r\n<ul>\r\n  {% for post in site.posts %}\r\n    <li>\r\n      <h2><a href=\"{{ post.url }}\">{{ post.title }}</a></h2>\r\n      <p>{{ post.excerpt }}</p>\r\n    </li>\r\n  {% endfor %}\r\n</ul>"
 add_file . blog.html "$BLOG_HTML" 'Blog'
 
 # Adding Contributors File
-CONTRIBUTORS_HTML="---\r\ntitle: Contributors\r\n---\r\n<h1>Contributors</h1>\r\n\r\n<ul>\r\n  {%% for contributor in site.contributors %%}\r\n    <li>\r\n      <h2><a href=\"{{ contributor.url }}\">{{ contributor.name }}</a></h2>\r\n      <h3>{{ contributor.position }}</h3>\r\n      <p>{{ contributor.content | markdownify }}</p>\r\n    </li>\r\n  {%% endfor %%}\r\n</ul>"
+CONTRIBUTORS_HTML="---\r\ntitle: Contributors\r\n---\r\n<h1>Contributors</h1>\r\n\r\n<ul>\r\n  {% for contributor in site.contributors %}\r\n    <li>\r\n      <h2><a href=\"{{ contributor.url }}\">{{ contributor.name }}</a></h2>\r\n      <h3>{{ contributor.position }}</h3>\r\n      <p>{{ contributor.content | markdownify }}</p>\r\n    </li>\r\n  {% endfor %}\r\n</ul>"
 add_file . contributors.html "$CONTRIBUTORS_HTML" 'Contributors'
 
 # Adding Index File
@@ -44,7 +44,7 @@ CSS="---\r\n---\r\n@import \"main\";"
 add_file ./assets/css styles.scss "$CSS" 'CSS'
 
 # Adding Contributors
-ADMIN_HTML="---\r\nname: Admin\r\nposition: Administrator\r\n---\r\n{{page.name}} the {{page.position}} is the creator and administrator of $APP_NAME.\r\n"
+ADMIN_HTML="---\r\nname: Admin\r\nposition: Administrator\r\n---\r\n{{ page.name }} the {{ page.position }} is the creator and administrator of $APP_NAME.\r\n"
 add_file ./_contributors Admin.md "$ADMIN_HTML" 'Contributors'
 
 # Adding Navigation Data
@@ -52,19 +52,19 @@ NAVIGATION_DATA="- name: Home\r\n  link: /$APP_NAME/index.html\r\n- name: About\
 add_file ./_data navigation.yml "$NAVIGATION_DATA" 'Navigation Data'
 
 # Adding Navigation HTML
-NAVIGATION_HTML="<nav>\r\n    {%% for item in site.data.navigation %%}\r\n        <a href=\"{{ item.link }}\" {%% if page.url == item.link %%}class=\"current\"{%% endif %%}>{{ item.name }}</a>\r\n    {%% endfor %%}\r\n</nav>\r\n"
+NAVIGATION_HTML="<nav>\r\n    {% for item in site.data.navigation %}\r\n        <a href=\"{{ item.link }}\" {% if page.url == item.link %}class=\"current\"{% endif %}>{{ item.name }}</a>\r\n    {% endfor %}\r\n</nav>\r\n"
 add_file ./_includes navigation.html "$NAVIGATION_HTML" 'Navigation HTML'
 
 # Adding Contributor Layout
-CONTRIBUTOR_LAYOUT="---\r\nlayout: default\r\n---\r\n<h1>{{ page.name }}</h1>\r\n<h2>{{ page.position }}</h2>\r\n\r\n{{ content }}\r\n\r\n<h2>Posts</h2>\r\n<ul>\r\n  {%% assign filtered_posts = site.posts | where: \'contributor\', page.name %%}\r\n  {%% for post in filtered_posts %%}\r\n    <li><a href=\"{{ post.url }}\">{{ post.title }}</a></li>\r\n  {%% endfor %%}\r\n</ul>"
+CONTRIBUTOR_LAYOUT="---\r\nlayout: default\r\n---\r\n<h1>{{ page.name }}</h1>\r\n<h2>{{ page.position }}</h2>\r\n\r\n{{ content }}\r\n\r\n<h2>Posts</h2>\r\n<ul>\r\n  {% assign filtered_posts = site.posts | where: \'contributor\', page.name %}\r\n  {% for post in filtered_posts %}\r\n    <li><a href=\"{{ post.url }}\">{{ post.title }}</a></li>\r\n  {% endfor %}\r\n</ul>"
 add_file ./_layouts contributor.html "$CONTRIBUTOR_LAYOUT" 'Contributor Layout'
 
 # Adding Default Layout
-DEFAULT_LAYOUT="<!doctype html>\r\n<html>\r\n  <head>\r\n    <meta charset=\"utf-8\">\r\n    <title>{{ page.title }}</title>\r\n    <link rel=\"stylesheet\" href=\"/assets/css/styles.css\">\r\n    {%% feed_meta %%}\r\n    {%% seo %%}\r\n  </head>\r\n  <body>\r\n    {%% include navigation.html %%}\r\n    {{ content }}\r\n  </body>\r\n</html>\r\n"
+DEFAULT_LAYOUT="<!doctype html>\r\n<html>\r\n  <head>\r\n    <meta charset=\"utf-8\">\r\n    <title>{{ page.title }}</title>\r\n    <link rel=\"stylesheet\" href=\"/assets/css/styles.css\">\r\n    {% feed_meta %}\r\n    {% seo %}\r\n  </head>\r\n  <body>\r\n    {% include navigation.html %}\r\n    {{ content }}\r\n  </body>\r\n</html>\r\n"
 add_file ./_layouts default.html "$DEFAULT_LAYOUT" 'Default Layout'
 
 # Adding Post Layout
-POST_LAYOUT="---\r\nlayout: default\r\n---\r\n<h1>{{ page.title }}</h1>\r\n<p>\r\n    Written by {{ page.contributor }} on {{ page.date | date_to_string }} \r\n    {%% assign contributor = site.contributors | where: \'name\', page.contributor %%}\r\n    {%% if contributor %%}\r\n        <a href=\"{{ contributor.url }}\">{{ contributor.name }}</a>\r\n    {%% endif %%}\r\n</p>\r\n\r\n{{ content }}\r\n"
+POST_LAYOUT="---\r\nlayout: default\r\n---\r\n<h1>{{ page.title }}</h1>\r\n<p>\r\n    Written by {{ page.contributor }} on {{ page.date | date_to_string }} \r\n    {% assign contributor = site.contributors | where: \'name\', page.contributor %}\r\n    {% if contributor %}\r\n        <a href=\"{{ contributor.url }}\">{{ contributor.name }}</a>\r\n    {% endif %}\r\n</p>\r\n\r\n{{ content }}\r\n"
 add_file ./_layouts post.html "$POST_LAYOUT" 'Post Layout'
 
 # Adding First Post
